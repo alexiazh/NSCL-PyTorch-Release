85c345d6f14e9b99721e8c10e922ad8decce53f5
Alexia Zhou <alexiazhou726@ucla.edu>
2024-01-31 21:59:43 -0800

    change .cuda() to .to('cuda')

--- scripts/scenegraph_test.py
+++ scripts/scenegraph_test.py
@@ -7,9 +7,9 @@ scene_graph = sng.SceneGraph(256, [None, 256, 256], 16)
 print(scene_graph)
 
 
-input = torch.rand(size=(1, 256, 16, 24))
-objects = torch.rand(size=(1, 4)).cuda()
-objects_length = torch.tensor([1]).cuda()
+input = torch.rand(size=(1, 256, 16, 24)).to("cuda")
+objects = torch.rand(size=(1, 4)).to("cuda")
+objects_length = torch.tensor([1]).to("cuda")
 
 
 print(scene_graph.forward(input, objects, objects_length))
