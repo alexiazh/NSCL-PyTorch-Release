554199cdd57c41ec8a403882632741716851cb1a
Alexia Zhou <alexiazhou726@ucla.edu>
2024-01-31 17:48:48 -0800

    add testing scripts

---
+++ scripts/scenegraph_test.py
@@ -0,0 +1,15 @@
+import torch
+
+import nscl.nn.scene_graph.scene_graph as sng
+
+
+scene_graph = sng.SceneGraph(256, [None, 256, 256], 16)
+print(scene_graph)
+
+
+input = torch.rand(size=(1, 256, 16, 24))
+objects = torch.rand(size=(1, 4))
+objects_length = torch.tensor([1])
+
+
+scene_graph.forward(input, objects, objects_length)
