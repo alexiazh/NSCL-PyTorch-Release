decd609c46eeaeff55214ee7a213430f2219c5e4
Alexia Zhou <alexiazhou726@ucla.edu>
2024-01-31 21:48:56 -0800

    add cuda back

--- scripts/scenegraph_test.py
+++ scripts/scenegraph_test.py
@@ -8,7 +8,7 @@ print(scene_graph)
 
 
 input = torch.rand(size=(1, 256, 16, 24))
-objects = torch.rand(size=(1, 4))
+objects = torch.rand(size=(1, 4)).cuda()
 objects_length = torch.tensor([1]).cuda()
 
 
