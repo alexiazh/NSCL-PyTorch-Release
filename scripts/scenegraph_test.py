c18314c5d27194090d091f3a4fdf876c0148f9f4
Alexia Zhou <alexiazhou726@ucla.edu>
2024-01-31 21:46:35 -0800

    remove cuda

--- scripts/scenegraph_test.py
+++ scripts/scenegraph_test.py
@@ -7,8 +7,8 @@ scene_graph = sng.SceneGraph(256, [None, 256, 256], 16)
 print(scene_graph)
 
 
-input = torch.rand(size=(1, 256, 16, 24)).cuda()
-objects = torch.rand(size=(1, 4)).cuda()
+input = torch.rand(size=(1, 256, 16, 24))
+objects = torch.rand(size=(1, 4))
 objects_length = torch.tensor([1]).cuda()
 
 
